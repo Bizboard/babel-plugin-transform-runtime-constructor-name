a774694f71bfb83dcae4add9ecab48d556426c56
Tom Clement <mr@tomclement.nl>
2016-06-29 17:13:32 +0200

    Changed runtime name property to a getter method

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /**
- * Adds a class property '_name' to Arva Controller and Model classes containing their 
-unmangled class names.
+ * Adds a class property '_name' to Arva Controller and Model classes containing their unmangled class names.
  *
  * @param t
  * @returns {{visitor: {Class: (function(*=))}}}
@@ -12,9 +11,10 @@ export default function({ types: t }) {
                 let superClassName = path.node.superClass.name;
                 if(superClassName === 'Controller' || superClassName === 'Model') {
                     let name = path.node.id.name;
-                    let nameProp = t.classProperty(t.Identifier('_name'), 
-t.stringLiteral(name), t.stringLiteralTypeAnnotation(), []);
-                    path.node.body.body.push(nameProp);
+                    let nameMethod = t.classMethod('get', t.Identifier('_name'), [], t.blockStatement([
+                        t.returnStatement(t.stringLiteral(name))
+                    ]));
+                    path.node.body.body.push(nameMethod);
                     path.replaceWith(path);
                 }
             }
